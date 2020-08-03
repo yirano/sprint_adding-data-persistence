93e9e81cf6a2c4b67e60fe282de574daadc17d4e
yirano <yirano@protonmail.ch>
2020-08-02 21:51:45 -0400

    List tasks and post new task

--- models/projects_model.js
+++ models/projects_model.js
@@ -9,8 +9,20 @@ async function addProject(data) {
     .insert(data)
 }
 
+async function getAllTasks() {
+  return await db('tasks')
+    .join('projects', 'projects.id', 'tasks.project_id')
+    .select('projects.name as projName', 'projects.desc as projDetail', 'tasks.desc as taskDetail')
+}
+
+async function addTask(data) {
+  return await db('tasks')
+    .insert(data)
+}
+
 module.exports = {
   getAllProjects,
   addProject,
-
+  addTask,
+  getAllTasks
 }
--- routers/projects_router.js
+++ routers/projects_router.js
@@ -10,10 +10,33 @@ router.get('/', async (req, res, next) => {
   }
 })
 
+router.get('/tasks', async (req, res, next) => {
+  try {
+    const tasks = await Projects.getAllTasks()
+    res.json(tasks)
+  } catch (error) {
+    next(error)
+  }
+})
+
 router.post('/', async (req, res, next) => {
   try {
     const addProject = await Projects.addProject(req.body)
     res.json(addProject)
+  } catch (error) {
+    next(error)
+  }
+})
+
+router.post('/:id/tasks', async (req, res, next) => {
+  const newTask = {
+    desc: req.body.desc,
+    notes: req.body.note,
+    project_id: req.params.id
+  }
+  try {
+    const addTask = await Projects.addTask(newTask)
+    res.json(addTask)
   } catch (error) {
     next(error)
   }
